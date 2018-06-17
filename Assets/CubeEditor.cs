8bcd004cefcf7e6d6189096883ef45544567cdab
15161153 <15161153@studentmail.ul.ie>
2018-06-18 00:10:05 +0100

    Cube Editor Script -Dynamic text label with position on top for pathfinding -[SectionBase] so it now defaults to move entire cube instead of quads -Saved scene as Level 1

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour {
+    [Range(1f, 20f)] [SerializeField] float gridsize = 10f;
+
+    TextMesh textmesh;
+    // Update is called once per frame
+    void Update () {
+        Vector3 snapPos;
+        
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridsize) * gridsize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridsize) * gridsize;
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textmesh = GetComponentInChildren<TextMesh>();
+        textmesh.text = snapPos.x / gridsize + "," + snapPos.z / gridsize;
+	}
+}
