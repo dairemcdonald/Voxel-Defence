b378e0cf06176aafac60e3cfeb94a3b2e8ecd13d
15161153 <15161153@studentmail.ul.ie>
2018-07-14 18:58:09 +0100

    - Enemy Spawner script -Bullets no longer bounce -Bullets are faster

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+    [SerializeField] float spawnDelay = 5f;
+    [SerializeField] int enemyNum = 4;
+    public EnemyMover prefab;
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        for (int i = 0; i < enemyNum; i++)
+        {
+            Instantiate(prefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+}
