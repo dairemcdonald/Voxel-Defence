dcddbcb02d1f1bf5e5c0cf805111ba21b7908135
15161153 <15161153@studentmail.ul.ie>
2018-07-15 20:38:01 +0100

    Post Processing Added

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -22,7 +22,7 @@ public class EnemySpawner : MonoBehaviour {
         {
             var tempInt = Instantiate(prefab, transform.position, Quaternion.identity);
             tempInt.transform.parent = this.transform;
-            waveText.text = (enemyNum - i).ToString(); //subtracts already spawned enemies from total expected
+            waveText.text = ((enemyNum - i)-1).ToString(); //subtracts already spawned enemies from total expected 
             yield return new WaitForSeconds(spawnDelay);
         }
     }
